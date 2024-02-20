b83009163447571c47417c32d5b4874660366964
Nery <pedronas@al.insper.edu.br>
2024-02-20 12:12:40 -0300

    server v2 - FEITO!

--- server.py
+++ server.py
@@ -19,15 +19,16 @@ while True:
     txLen = com1.rx.getBufferLen()
     rxBuffer, nRx = com1.getData(txLen)
     if nRx != 0:
-        print(f"rxBuffer: {rxBuffer}")
-        contador += 1
-        print("Comando recebido!")
-        print(f"contador: {contador}")
-        if rxBuffer == b'\x88\x99':
+        if rxBuffer != b'\x88\x99':
+            print(f"rxBuffer: {rxBuffer}")
+            contador += 1
+            print("Comando recebido!")
+            print(f"contador: {contador}")
+        elif rxBuffer == b'\x88\x99':
             break
 
-resposta = bytes([contador - 1])
+resposta = bytes([contador])
 # Caso de erro proposital hard coded
 # resposta = bytes([contador + 5])
 com1.sendData(bytearray(resposta))
-print('Resposta enviada: {}' .format(resposta))
+print('\033[94mResposta enviada: {}\033[0m' .format(resposta))
