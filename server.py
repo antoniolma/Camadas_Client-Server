6d734e995cbd06c4680ac413598acdf58fa930de
Nery <pedronas@al.insper.edu.br>
2024-02-22 15:32:22 -0300

    server done

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from enlace import *
 import time
-import numpy as np
 
 serialName = "COM8"
 contador = 0
@@ -19,16 +18,19 @@ while True:
     txLen = com1.rx.getBufferLen()
     rxBuffer, nRx = com1.getData(txLen)
     if nRx != 0:
-        if rxBuffer != b'\x88\x99':
-            print(f"rxBuffer: {rxBuffer}")
-            contador += 1
-            print("Comando recebido!")
-            print(f"contador: {contador}")
-        elif rxBuffer == b'\x88\x99':
+        if rxBuffer == b'\x88\x99':
             break
+        else:
+            contador += rxBuffer.count(b'\x22')
+            comandos = rxBuffer.rsplit(b'\x22')
 
 resposta = bytes([contador])
 # Caso de erro proposital hard coded
 # resposta = bytes([contador + 5])
 com1.sendData(bytearray(resposta))
+
 print('\033[94mResposta enviada: {}\033[0m' .format(resposta))
+print("Comandos recebidos:")
+
+for comando in comandos[:len(comandos)-1]:
+    print(comando)
